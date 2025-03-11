e18ea1545c18c252517dd9505876eee89e85bb86
r-muresan <robert.victor.muresan@gmail.com>
2025-03-10 22:37:52 -0700

    Initial commit

---
+++ apps/bequest-interview-question-v2/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="bequest-interview-question-v2" />
+    </div>
+  );
+}
+
+export default App;
